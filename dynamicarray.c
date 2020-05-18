63600de7436cced29765e0fc21e87265cc51e931
anubhavrajput70 <48093685+anubhavrajput70@users.noreply.github.com>
2020-05-18 16:55:02 +0545

    Add files via upload

---
+++ dynamicarray.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+void main()
+{
+int i,n,*arr;
+printf("enter the size of the array=");
+scanf("%d",&n);
+arr=(int*)calloc(n,sizeof(int));
+printf("enter the element of the array=");
+for(i=0;i<n;i++)
+{
+scanf("%d",(arr+i));
+}
+printf("array elements are=");
+for(i=0;i<n;i++)
+{
+printf(" %d",*(arr+i));
+}
+}
+
